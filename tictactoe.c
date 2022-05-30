f1b8b94fd0ad62068d93a7bb78cd431cf6cb2ee3
Emil <60893397+Vorsteh@users.noreply.github.com>
2022-05-30 22:49:26 +0200

    Update tictactoe.c

--- tictactoe.c
+++ tictactoe.c
@@ -1,73 +1,200 @@
 #include <stdio.h>
-#include <assert.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <time.h>
 #include <string.h>
 
-#define N 2048
+char board[3][3];
+const char PLAYER = 'X';
+const char COMPUTER = 'O';
 
-struct inst{
-  int num;
-  char dir[10];
-};
+void resetBoard();
+void printBoard();
+int checkFreeSpaces();
+void playerMove();
+void computerMove();
+char checkWinner();
+void printWinner(char);
+
+void saveGame(int pWin, int cWin);
 
 
-struct inst xs[N];
-int xs_size = 0;
 
-int main(int argc, char const *argv[]) {
-  assert(argc == 2 && "Provide input => input.txt path");
+int main()
+{
+  char winner;
+  int cWin;
+  int pWin;
+  FILE *file = fopen("C:\\Users\\Rwy\\Documents\\C\\stats", "r+");
+  char line[12] = "";
+  int i = 0;
+  char playAgain[3];
 
-  printf("Input file: %s\n", argv[1]);
-  FILE *file = fopen(argv[1], "r");
-
-  int number = 0;
-  char str[10] = "";
-
-  while(fscanf(file, "%s", str) == 1 && fscanf(file, "%d", &number) == 1){
-    xs[xs_size].num = number;
-    strcpy(xs[xs_size++].dir, str);
+  while(fgets(line, 12, file) != NULL){
+    i++;
+    if(i == 1){
+      pWin = atoi(line);
+    }else if(i == 2){
+      cWin = atoi(line);
+    }
   }
 
 
-  //part 1
-  {
-    int down = 0;
-    int forward = 0;
 
-    for(int i = 0; i < xs_size; i++){
-      if(strcmp(xs[i].dir, "up") == 0){
-        down -= xs[i].num;
-      }else if(strcmp(xs[i].dir, "down") == 0){
-        down += xs[i].num;
-      }else if(strcmp(xs[i].dir, "forward") == 0){
-        forward += xs[i].num;
-      }else{
-        printf("Unknown dir: %s at: %d", xs[i].dir, i);
+  fclose(file);
+  resetBoard();
+
+  while(1){
+    resetBoard();
+    winner = ' ';
+    printf("|----------------|\n");
+    printf("pWIn: %d\n", pWin);
+    printf("cWIn: %d\n", cWin);
+    printf("|----------------|\n");
+    while(winner == ' ' && checkFreeSpaces() != 0){
+      printBoard();
+
+      playerMove();
+      winner = checkWinner();
+
+      if(winner != ' ' || checkFreeSpaces() == 0){
+        break;
+      }
+
+      computerMove();
+      winner = checkWinner();
+
+      if(winner != ' ' || checkFreeSpaces() == 0){
+        break;
       }
     }
-    printf("\nCount 1: %d", down * forward);
-  }
+    printBoard();
+    printWinner(winner);
 
-  //part 2
-  {
-    int down = 0;
-    int forward = 0;
-    int aim = 0;
-
-    for (size_t i = 0; i < xs_size; i++) {
-      if (strcmp(xs[i].dir, "up") == 0) {
-        aim -= xs[i].num;
-      } else if (strcmp(xs[i].dir, "down") == 0) {
-        aim += xs[i].num;
-      } else if (strcmp(xs[i].dir, "forward") == 0) {
-        forward += xs[i].num;
-        down += aim * xs[i].num;
-      } else {
-        printf("Unknown dir: %s at: %d", xs[i].dir, i);
-      }
+    if(winner == PLAYER){
+      pWin++;
+    }else{
+      cWin++;
     }
-    printf("\nCount 2: %d", down * forward);
+
+    printf("Do you want to play again? (Yes/No)\n");
+    scanf("\n%s", &playAgain);
+    if(strcmp(strlwr(playAgain), "no") == 0){
+      break;
+    }
   }
 
-
+  saveGame(pWin, cWin);
   return 0;
+}
+
+
+
+
+void resetBoard(){
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      board[i][j] = ' ';
+    }
+  }
+}
+void printBoard(){
+  printf(" %c | %c | %c ", board[0][0], board[0][1], board[0][2]);
+  printf("\n---|---|---\n");
+  printf(" %c | %c | %c ", board[1][0], board[1][1], board[1][2]);
+  printf("\n---|---|---\n");
+  printf(" %c | %c | %c ", board[2][0], board[2][1], board[2][2]);
+  printf("\n");
+
+}
+int checkFreeSpaces(){
+  int freeSpaces = 9;
+
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      if(board[i][j] != ' '){
+        freeSpaces--;
+      }
+    }
+  }
+  return freeSpaces;
+}
+void playerMove(){
+  int x;
+  int y;
+
+  do{
+    printf("Enter row number (1-3): ");
+    scanf("%d", &x);
+    x--;
+    printf("Enter column number (1-3): ");
+    scanf("%d", &y);
+    y--;
+
+    if(board[x][y] != ' '){
+      printf("Invalid move!\n");
+    }else{
+      board[x][y] = PLAYER;
+      break;
+    }
+  }while(board[x][y] != ' ');
+}
+void computerMove(){
+  //random num
+  srand(time(0));
+
+  int x;
+  int y;
+
+  if(checkFreeSpaces() > 0){
+    do{
+      x = rand() % 3;
+      y = rand() % 3;
+    }while (board[x][y] != ' ');
+
+    board[x][y] = COMPUTER;
+  }else{
+    printWinner(' ');
+  }
+}
+char checkWinner(){
+  //check rows
+  for(int i = 0; i < 3; i++){
+    if(board[i][0] == board[i][1] && board[i][0] == board[i][2]){
+        return board[i][0];
+    }
+  }
+
+  //check columns
+  for(int i = 0; i < 3; i++){
+    if(board[0][i] == board[1][i] && board[0][i] == board[2][i]){
+        return board[0][i];
+    }
+  }
+
+  //check diagonals
+  if(board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+    return board[0][0];
+  }
+  if(board[0][2] == board[1][1] && board[0][2] == board[2][0]){
+    return board[0][2];
+  }
+
+  return ' ';
+}
+void printWinner(char winner){
+  if(winner == PLAYER){
+    printf("YOU WIN!\n");
+  }else if(winner == COMPUTER){
+    printf("YOU LOSE!\n");
+  }else{
+    printf("ITS A TIE!\n");
+  }
+}
+
+void saveGame(int pWin, int cWin){
+  FILE *file = fopen("C:\\Users\\Rwy\\Documents\\C\\stats", "w+");
+  fprintf(file, "%d\n%d", pWin, cWin);
+
+  fclose(file);
 }
